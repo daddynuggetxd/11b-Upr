3586cbfc5b2ccc7fe103b073f61fed20a8e8a08a
m-petrov <donutpinkz@gmail.com>
2022-02-09 08:28:29 +0200

    zadacha masivi

---
+++ masiviZad1/masiviZad1.1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace masiviZad1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = Console.ReadLine().Split().ToArray();
+            while (true)
+            {
+                var cmd = Console.ReadLine().Split().ToArray();
+                string command = cmd[0];
+                if (command == "END")
+                {
+                    break;
+                }
+                switch (command)
+                {
+                    case "Reverse":
+                        arr = arr.Reverse().ToArray();
+                        break;
+                    case "Distinct":
+                        arr = arr.Distinct().ToArray();
+                        break;
+                    case "Replace":
+                        int index = int.Parse(cmd[1]);
+                        string newString = cmd[2];
+                        if (index >= 0 && index < arr.Length)
+                        {
+                            arr[index] = newString;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input!");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Invalid command!");
+                        break;
+                }
+            }
+            Console.WriteLine(string.Join(", ", arr));
+        }
+       
+        }
+    }
+
---
+++ masiviZad1/masiviZad1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace masiviZad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split().ToArray();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            { string[] cmd = Console.ReadLine().Split().ToArray();
+
+                string command = cmd[0];
+                switch (command)
+                {
+                    case "Reverse": arr = arr.Reverse().ToArray();
+                        break;
+                    case "Distinct": arr = arr.Distinct().ToArray();
+                        break;
+                    case "Replace": int index = int.Parse(cmd[1]);
+                        string newString = cmd[2];
+                        arr[index] = newString;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            Console.WriteLine(string.Join(", ", arr));
+
+        }
+    }
+}
