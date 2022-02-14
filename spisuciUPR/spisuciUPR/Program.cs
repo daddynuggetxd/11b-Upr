9807f9bff972ffa53e77395c1a445071a99f46d6
m.petrov <donutpinkz@gmail.com>
2022-02-14 09:18:59 +0200

    zadachi sus spisuci

---
+++ spisuciUPR/spisuciUPR/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace spisuciUPR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+           
+
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                if (input[0].ToLower()== "end")
+                {
+                    break;
+                }
+
+                if (input[0].ToLower() == "add")
+                {
+                    numbers.Add(int.Parse(input[1]));
+                }
+                if (input[0].ToLower() == "remove" )
+                {
+                    if (numbers.Contains(int.Parse(input[1])))
+                    {
+                        numbers.Remove(int.Parse(input[1]));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Specified number isn't in the list");
+                    }
+                }
+                if (input[0].ToLower() == "removeat")
+                {
+                    numbers.RemoveAt(int.Parse(input[1]));
+                }
+                if (input[0].ToLower() == "insert")
+                {
+                    numbers.Insert(int.Parse(input[2]), int.Parse(input[1]));
+                }
+               
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
---
+++ spisuciUPR/zad2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace zad2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> endResut = new List<int>();
+
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                
+
+                if (input[0].ToLower() == "delete")
+                {
+                    while (numbers.Contains(int.Parse(input[1])))
+                    {
+                        numbers.Remove(int.Parse(input[1]));
+                    }
+                    
+                }
+
+                if (input[0].ToLower() == "insert")
+                {
+                    numbers.Insert(int.Parse(input[2]), int.Parse(input[1]));
+                }
+
+                if (input[0].ToLower() == "even")
+                {
+                    foreach (var num in numbers)
+                    {
+                        if (num % 2 == 0 )
+                        {
+                            endResut.Add(num);
+                        }
+                    }
+                    Console.WriteLine(string.Join(" ", endResut));
+                    break;
+                }
+
+                if (input[0].ToLower() == "odd")
+                {
+                    foreach (var num in numbers)
+                    {
+                        if (num %2 != 0)
+                        {
+                            endResut.Add(num);
+                        }
+                    }
+                    Console.WriteLine(string.Join(" ", endResut));
+                    break;
+                    
+                }
+            }
+
+
+        }
+    }
+}
