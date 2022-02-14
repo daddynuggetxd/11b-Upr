6d2b9c7a66bcf667341ca9eabfe6cb33346d4796
m.petrov <donutpinkz@gmail.com>
2022-02-14 10:37:10 +0200

    vtorata zadacha za dneska

---
+++ Podgotovka/Podgotovka/Program.cs
@@ -0,0 +1,82 @@
+﻿using NCalc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Podgotovka
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                if (input[0].ToLower() == "add")
+                {
+                    if (!numbers.Contains(int.Parse(input[2])))
+
+                    {
+                        numbers.Insert(int.Parse(input[1]), int.Parse(input[2])); 
+                    }
+                }
+
+                if (input[0].ToLower() == "contains")
+                {
+                    if (numbers.Contains(int.Parse(input[1])))
+                    {
+                        Console.WriteLine(numbers.IndexOf(int.Parse(input[1])));
+                    }
+                    else
+                    {
+                        Console.WriteLine(-1);
+                    }
+                }
+
+                if (input[0].ToLower() == "swap")
+                {
+                    int element1 = numbers.IndexOf(int.Parse(input[1]));
+                    int element2 = numbers.IndexOf(int.Parse(input[2]));
+
+                    int temp = numbers[element1];
+                    numbers[element1] = numbers[element2];
+                    numbers[element2] = temp;
+                }
+
+                if (input[0].ToLower() == "remove")
+                {
+
+                    if (int.Parse(input[1]) >= 0)
+                    {
+                        numbers.RemoveAt(int.Parse(input[1]));
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("index is not correct");
+                    }
+                    
+                }
+                if (int.TryParse(input[0],out int result) == true)
+                {
+                    numbers.Sort();
+                    Console.WriteLine(string.Join(" ", numbers));
+                }
+
+                if (input[0].ToLower() == "print" )
+                {
+                    Console.WriteLine(string.Join(" ", numbers));
+                    break;
+                }
+            }
+        }
+               
+    }
+
+}
+
+
+    
